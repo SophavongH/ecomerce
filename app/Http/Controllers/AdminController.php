46ee08cef87895da8c68cb81feaf714cb425f4ca
SophavongH <89271701+SophavongH@users.noreply.github.com>
2023-02-22 13:53:55 +0700

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -94,7 +94,7 @@ class AdminController extends Controller
 
     public function Order(){
         $order = Order::all();
-        return view('admin.Order', compact('Order'));
+        return view('admin.order', compact('order'));
     }  
     public function delivered($id){
         $order = order::find($id);
